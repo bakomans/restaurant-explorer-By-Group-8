c1bebee6571a2cee560f281ebbe45c3e9eab5236
bakomans <bakomans@gmail.com>
2023-11-30 13:36:46 +0000

    changes in main files try to add next api keys

--- main.js
+++ main.js
@@ -52,3 +52,75 @@ function showModal(message) {
 document.addEventListener('DOMContentLoaded', function () {
     searchRestaurants(); 
 });
+async function fetchNews() {
+    const newsUrl = 'https://newsapi.org/v2/top-headlines';
+    const apiKey = '87611144aed846cb8cda5579a2759866';
+
+    const country = 'us';
+    const url = `${newsUrl}?country=${country}&apiKey=${apiKey}`;
+
+    try {
+        const response = await fetch(url);
+        const result = await response.json();
+
+        // Sprawdź, czy 'articles' istnieje przed próbą iteracji
+        if (result.articles && result.articles.length > 0) {
+            displayNews(result.articles);
+        } else {
+            console.error('No articles found in the response:', result);
+        }
+    } catch (error) {
+        console.error('Error fetching news:', error);
+    }
+}
+
+// Funkcja do wyświetlania aktualnych wiadomości na stronie
+function displayNews(newsArray) {
+    const newsListContainer = document.getElementById('newsList');
+
+    // Wyczyść zawartość kontenera przed dodaniem nowych wiadomości
+    newsListContainer.innerHTML = '';
+
+    newsArray.forEach(news => {
+        const newsItem = document.createElement('div');
+        newsItem.classList.add('col-md-4', 'mb-4');
+
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const img = document.createElement('img');
+        img.src = news.urlToImage || 'https://via.placeholder.com/150';
+        img.alt = 'News Image';
+        img.classList.add('card-img-top');
+
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+
+        const title = document.createElement('h5');
+        title.classList.add('card-title');
+        title.textContent = news.title;
+
+        const description = document.createElement('p');
+        description.classList.add('card-text');
+        description.textContent = news.description;
+
+        const readMoreLink = document.createElement('a');
+        readMoreLink.href = news.url;
+        readMoreLink.target = '_blank';
+        readMoreLink.classList.add('btn', 'btn-primary');
+        readMoreLink.textContent = 'Read More';
+
+        cardBody.appendChild(title);
+        cardBody.appendChild(description);
+        cardBody.appendChild(readMoreLink);
+
+        card.appendChild(img);
+        card.appendChild(cardBody);
+
+        newsItem.appendChild(card);
+        newsListContainer.appendChild(newsItem);
+    });
+}
+
+// Wywołaj funkcję pobierającą i wyświetlającą aktualne wiadomości
+fetchNews();
